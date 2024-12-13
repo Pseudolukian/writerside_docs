fc0f1c67e34a12e137f362cff63998e3836d5fb6
Pseudolukian <pynanist@gmail.com>
2024-12-13 15:42:59 +0100

    Add prod workflow

--- .github/scripts/s3_upload.py
+++ .github/scripts/s3_upload.py
@@ -1,22 +1,12 @@
 import boto3
 import sys
 import os
-import zipfile
 
 #======== Set up zone ================#
 bucket_name = 'writerside-demo-docs'
 yandex_s3_endpoint = "https://storage.yandexcloud.net"
 
 #=====================================#
-
-def unzip_file(zip_path, extract_to):
-    
-    if not os.path.exists(extract_to):
-        os.makedirs(extract_to)
-
-    with zipfile.ZipFile(zip_path, 'r') as zip_ref:
-        zip_ref.extractall(extract_to)
-        print(f"Файлы извлечены в: {extract_to}")
 
 
 def get_s3_session(access_key_id, secret_access_key):
@@ -64,17 +54,15 @@ def upload_doc_s3(s3_session, bucket_name, folder_path):
             print(f"Загружен {local_path} в s3://{bucket_name}/{s3_key}")
     print("Все файлы успешно загружены.")
 
-def run(zip_archive_path, folder_to_unzip, access_key_id, secret_access_key):
-    unzip_file(zip_path = zip_archive_path, extract_to=folder_to_unzip)
+def run(documentation_folder, access_key_id, secret_access_key):
     s3_connect = get_s3_session(access_key_id=access_key_id, secret_access_key = secret_access_key)
     clean_s3_bucket(s3_session=s3_connect)
-    upload_doc_s3(s3_session=s3_connect, bucket_name=bucket_name, folder_path=folder_to_unzip)
+    upload_doc_s3(s3_session=s3_connect, bucket_name=bucket_name, folder_path=documentation_folder)
 
 if __name__ == "__main__":
-    run(zip_archive_path=sys.argv[1], 
-        folder_to_unzip=sys.argv[2], 
-        access_key_id = sys.argv[3], 
-        secret_access_key = sys.argv[4])
+    run(documentation_folder=sys.argv[1], 
+        access_key_id = sys.argv[2], 
+        secret_access_key = sys.argv[3])
 
     
 
